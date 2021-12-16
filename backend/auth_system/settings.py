276dd280a3696f2df50543f0f47f15df26ff4aa2
Akash <92708222+HimmatPanchal@users.noreply.github.com>
2021-12-16 19:39:45 +0530

    from backend side
    Edited the credential

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'auth_system',
         'USER': 'postgres',
-        'PASSWORD': '[YOUR DATABASE PASSWORD]',
+        'PASSWORD': 'akas100896',
         'HOST': 'localhost'
     }
 }
@@ -96,8 +96,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '[YOUR EMAIL THAT WILL SEND]'
-EMAIL_HOST_PASSWORD = '[YOUR EMAIL APP PASSWORD]'
+EMAIL_HOST_USER = 'akashvermavidhaninfotech@gmail.com'
+EMAIL_HOST_PASSWORD = 'Verma@_817'
 EMAIL_USE_TLS = True
 
 # Password validation
